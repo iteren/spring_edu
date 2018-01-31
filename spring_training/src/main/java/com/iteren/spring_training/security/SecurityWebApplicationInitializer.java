4a5dc53e5eb0bb110bffdef40d8bbf8234db0861
iteren <teigro@gmail.com>
2018-01-31 11:25:32 +0200

    security

---
+++ spring_training/src/main/java/com/iteren/spring_training/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.iteren.spring_training.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	public SecurityWebApplicationInitializer() {
+		super(SecurityConfig.class);
+	}
+}
