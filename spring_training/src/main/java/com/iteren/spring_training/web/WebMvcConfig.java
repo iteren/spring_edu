9032f9b31806f1a0250e38f35c45756e69e8d88d
iteren <iteren@CLATTER.softservecom.com>
2018-02-06 19:14:03 +0200

    index.html

--- spring_training/src/main/java/com/iteren/spring_training/web/WebMvcConfig.java
+++ spring_training/src/main/java/com/iteren/spring_training/web/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -14,6 +15,9 @@ import com.iteren.spring_training.web.filter.LogFilter;
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
+			"classpath:/resources/", "classpath:/static/", "classpath:/public/" };
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -27,5 +31,15 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 		bean.setPrefix("/WEB-INF/view/");
 		bean.setSuffix(".jsp");
 		return bean;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		if (!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		}
+		if (!registry.hasMappingForPattern("/**")) {
+			registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+		}
 	}
 }
